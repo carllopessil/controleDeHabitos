d215c3d85bbcf72383aff963796787be18721a04
carllopessil <carlosandrade60@hotmail.com>
2023-02-23 11:12:19 -0300

    incluindo cometarios na classe de teste

--- src/teste.java
+++ src/teste.java
@@ -2,7 +2,7 @@ public class teste {
     public static void main(String[] args) throws Exception {
         Vetor vetor = new Vetor(5);
 
-
+        //testando inserção/população do vetor
         try{
             vetor.adiciona("elemento jk");
             vetor.adiciona("elemento b");
@@ -16,15 +16,19 @@ public class teste {
             e.printStackTrace();
         }
 
+        //testando adição de elemento no inicio do vetor, posição 0
         System.out.println(vetor.adicionaInicio(0, "elemento-a"));
 
+        //identificando o tamanho do vetor
         System.out.println(vetor.tamanho());
 
+        //imprimindo os elementos do vetor
         System.out.println(vetor.toString());
 
-
+        //buscando elemento por posição
         System.out.println(vetor.busca(2));
 
+        //buscando posição por elemento
         System.out.println(vetor.busca1("elemento c"));
 
 
